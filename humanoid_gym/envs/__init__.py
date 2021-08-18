241d35c50a017e1858abee20916210792fa62515
0aqz0 <aqz@zju.edu.cn>
2021-08-18 21:09:54 +0800

    add nao real robot env

--- humanoid_gym/envs/__init__.py
+++ humanoid_gym/envs/__init__.py
@@ -2,3 +2,4 @@ from humanoid_gym.envs.pepper_env import PepperEnv
 from humanoid_gym.envs.nao_env import NaoEnv
 from humanoid_gym.envs.romeo_env import RomeoEnv
 from humanoid_gym.envs.dancer_env import DancerEnv
+from humanoid_gym.envs.nao_env_real import NaoEnvReal
